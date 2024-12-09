b5088803f5d4b913246fc83f58f50328d75e0b77
Alexey Krainev <blend.beats0w@icloud.com>
2024-12-10 03:36:04 +0500

    Прокси для Loon

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,6 @@ new Elysia()
         if (!Bun.env.PROXY_URL || !Bun.env.PROXY_TOKEN || Bun.env.PROXY_TOKEN !== query.token) return "Нет токена";
         const request = await fetch(Bun.env.PROXY_URL + Bun.env.PROXY_TOKEN);
         const proxy = await request.json();
-
-        const fileContent = [
-            "proxies:",
-            `\n  - name: '${proxy.tag}'`,
-            "\n    type: ss",
-            `\n    server: ${proxy.server}`,
-            `\n    port: ${proxy.server_port}`,
-            `\n    cipher: ${proxy.method}`,
-            `\n    password: '${proxy.password}'`,
-        ];
-        const file = Bun.file("proxy.yaml");
-        await Bun.write(file, fileContent.join(""));
-        return file;
+        return `Shadowsocks,${proxy.server},${proxy.server_port},${proxy.method},"${proxy.password}"`;
     })
     .listen(Bun.env.SERVER_PORT ?? 3000);
