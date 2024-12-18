1729ee8e8186bfea24470f4edb5000ecece24d57
Alexey Krainev <blend.beats0w@icloud.com>
2024-12-18 22:27:49 +0500

    Для нескольких прокси

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import { Elysia } from "elysia";
 new Elysia()
     .all("/", () => "Пусто ещё")
     .get("/proxy", async ({ query }) => {
-        if (!Bun.env.PROXY_URL || !Bun.env.PROXY_TOKEN || Bun.env.PROXY_TOKEN !== query.token) return "Нет токена";
+        if (!Bun.env.PROXY_URL || !Bun.env.PROXY_TOKEN) return "Нет переменных";
+        if (query.token !== Bun.env.TOKEN) return "Неверный токен";
+        if (!query.app) return "Неверное приложение"
+
         const request = await fetch(Bun.env.PROXY_URL + Bun.env.PROXY_TOKEN);
         const proxy = await request.json();
-        return `${proxy.tag} = Shadowsocks,${proxy.server},${proxy.server_port},${proxy.method},"${proxy.password}"`;
+
+        if (query.app === "loon") return `${proxy.tag} = Shadowsocks,${proxy.server},${proxy.server_port},${proxy.method},"${proxy.password}"`;
+        else if (query.app === "surge") return `[Proxy]\n${proxy.tag} = ss, ${proxy.server}, ${proxy.server_port}, encrypt-method=${proxy.method}, password=${proxy.password}, tfo=true, udp-relay=true`;
+        else return "Неизвестное приложение"
     })
     .listen(Bun.env.SERVER_PORT ?? 3000);
